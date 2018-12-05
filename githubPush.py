797ac4bd3af878b23032c260148e85e4a526dd9b
willie <chi8857@gmail.com>
2018-12-05 12:01:56 +0800

    add

---
+++ githubPush.py
@@ -0,0 +1,15 @@
+import os
+
+#if os.path.isfile(os.path.abspath(os.path.join(os.getcwd(), "..")) + r'\fileName.txt'): 
+	#with open(os.path.abspath(os.path.join(os.getcwd(), "..")) + r'\fileName.txt', 'r') as file :
+		#fileName = file.readline().strip()
+		#fileName = fileName.split(',')
+		#fileName = 'git add ' + fileName[2]
+		#os.system(fileName)
+		#os.system(r'git commit -m "add"')
+		#os.system(r'git push')
+		
+
+os.system(r'git add .')
+os.system(r'git commit -m "add"')
+os.system(r'git push')
